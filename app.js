efaf302eecf35657331cca0f983d7ff1195a8c52
emoose <abc@cock.li>
2014-06-16 06:48:46 +0100

    wip zombie mode, todo: learn how to do bug-free 2d collision detection

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ var jsdata = fs.readFileSync(__dirname + '/jquery.js');
 var sourcedata = fs.readFileSync(__dirname + '/app.js');
 
 var server = http.createServer(function(req, res) {
-    if(req.url !== '/jquery.js' && req.url !== '/app.js')
+    if(req.url === '/devindex.html')
+    {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end(fs.readFileSync(__dirname + '/index.html'));
+    } else if(req.url !== '/jquery.js' && req.url !== '/app.js')
     {
         res.writeHead(200, { 'Content-type': 'text/html'});
         res.end(indexdata);
@@ -24,37 +28,150 @@ var server = http.createServer(function(req, res) {
 });
 
 var players = [];
-var player_id = 1;
- 
-socketio.listen(server).on('connection', function (socket) {
+var player_id = 0;
+
+var io = socketio.listen(server);
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+var startTime = 0;
+
+function getConnectedPlayerCount()
+{
+    var count = 0;
+    for(p=0;p<players.length;p++)
+    {
+        if(players[p].connected !== 1)
+            continue;
+        count++;
+    }
+    return count;
+}
+
+function resetGame()
+{
+    var curTime = Math.round(+new Date()/1000);
+    var roundLimit = getConnectedPlayerCount() * 30;
+    if(curTime - startTime < roundLimit)
+    {
+        setTimeout(function() { resetGame(); }, (roundLimit - (curTime - startTime)) * 1000);
+        return;
+    }
+    for(p=0;p<players.length;p++)
+    {
+        players[p].monster = 0;
+        io.sockets.emit('updatePlayer', {id: players[p].id, x: players[p].x, y: players[p].y, monster: 0});
+    }
+    setTimeout(function(){ infectRandomPlayer(); }, 15 * 1000);
+}
+function infectRandomPlayer()
+{
+    var playeridx = randomInt(1, player_id);
+    for(p=0;p<players.length;p++)
+    {
+        if(players[p].id !== playeridx)
+            continue;
+
+        players[p].monster = 1;
+        io.sockets.emit('updatePlayer', {id: players[p].id, x: players[p].x, y: players[p].y, monster: 1, connected: players[p].connected});
+        break;
+    }
+    startTime = Math.round(+new Date()/1000);
+    setTimeout(function() { resetGame(); }, 60 * 1000);
+}
+
+setTimeout(function(){ infectRandomPlayer(); }, 60 * 1000);
+
+var allClients = [];
+
+io.on('connection', function (socket) {
+    allClients.push(socket);
+    
     socket.on('ID', function (msg) {
         console.log('connection from ID: ', msg);
-        var pid = player_id;
         player_id++;
-        players.push({id:pid,x:0,y:0})
+        var pid = player_id;
         
+        var randX = randomInt(0, 98);
+        var randY = randomInt(0, 98);
+        
+        players.push({id:pid,ip:socket.handshake.address,x:(randX*10),y:(randY*10),monster:0,connected:1,iosock:socket});
         socket.emit('ID', { ID: pid });
-        socket.broadcast.emit('newPlayer', {id: pid, x: 0, y: 0});
+        socket.broadcast.emit('newPlayer', {id: pid, x:(randX*10),y:(randY*10),monster:0,connected:1});
         
         for(p=0;p<players.length;p++)
         {
-            socket.emit('newPlayer', {id: players[p].id, x: players[p].x, y: players[p].y});
+            socket.emit('newPlayer', {id: players[p].id, x: players[p].x, y: players[p].y, monster: players[p].monster, connected: players[p].connected});
         }
     });
     
     socket.on('position', function (msg) {
+        if(msg.id > player_id)
+        {
+            socket.emit('refresh', {time:'now'});
+            return;
+        }
         console.log('position: ', msg);
-        if(msg.x < 500 && msg.y < 500)
+        if(msg.x < 1000 && msg.y < 1000)
         {
             for(p=0;p<players.length;p++)
             {
                 if(players[p].id !== msg.id)
                     continue;
+                if(players[p].ip !== socket.handshake.address)
+                    break;
                 players[p].x = msg.x;
                 players[p].y = msg.y;
+                if(players[p] !== undefined)
+                    socket.broadcast.emit('updatePlayer', {id: msg.id, x: msg.x, y: msg.y, monster: players[p].monster});
+                if(players[p] !== undefined)
+                    socket.emit('updatePlayer', {id: msg.id, x: msg.x, y: msg.y, monster: players[p].monster});
+                break;
             }
-            socket.broadcast.emit('updatePlayer', {id: msg.id, x: msg.x, y: msg.y});
-            socket.emit('updatePlayer', {id: msg.id, x: msg.x, y: msg.y});
+        }
+    });
+    
+    socket.on('disconnect', function() {
+        for(p=0;p<players.length;p++)
+        {
+            if(players[p].iosock !== socket)
+                continue;
+            console.log('player ' + players[p].id + ' disconnected');
+            players[p].connected = 0;
+            io.sockets.emit('updatePlayer', {id: players[p].id, x: players[p].x, y: players[p].y, monster: players[p].monster, connected: players[p].connected});
+            break;
+        }
+    });
+    
+    socket.on('infect', function(msg) {
+        if(msg.id > player_id)
+        {
+            socket.emit('refresh', {time:'now'});
+            return;
+        }
+        console.log('infect: ', msg);
+        
+        var ismonster = 0;
+        for(p=0;p<players.length;p++)
+        {
+            if(players[p].id !== msg.id)
+                continue;
+            ismonster = players[p].monster == 1;
+            break;
+        }
+        
+        if(!ismonster)
+            return;
+            
+        for(p=0;p<players.length;p++)
+        {
+            if(players[p].id !== msg.victimid)
+                continue;
+            players[p].monster = 1;
+            socket.broadcast.emit('updatePlayer', {id: msg.victimid, x: players[p].x, y: players[p].y, monster: players[p].monster});
+            socket.emit('updatePlayer', {id: msg.victimid, x: players[p].x, y: players[p].y, monster: players[p].monster});
+            break;
         }
     });
 });
