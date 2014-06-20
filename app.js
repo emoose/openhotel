967c6a556b4148d70a2b50aca1f7a0636003a1e6
infoburp <gwolfendale@gmail.com>
2014-06-20 12:52:44 +0100

    any file in /public can be requested via http, all js files are stored locally now

--- app.js
+++ app.js
@@ -131,31 +131,43 @@ git.short(function(str)
     console.log('running openhotel version ' + ver);
 });
 
-var server = http.createServer(function(req, res) {
-    if(req.url === '/devindex.html')
-    {
-        res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end(fs.readFileSync(__dirname + '/public/index.html'));
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8080;
+
+var server = http.createServer(function(request, response) {
+
+  var uri = "./public" + url.parse(request.url).pathname 
+    , filename = path.join(process.cwd(), uri);
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
     }
-    else if(req.url === '/style.css')
-    {
-        res.writeHead(200, { 'Content-type': 'text/css'});
-        res.end(fs.readFileSync(__dirname + '/public/style.css'));
-    }
-    else if(req.url !== '/app.js')
-    {
-        res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end(indexdata);
-    }
-    else
-    {
-        res.writeHead(200, { 'Content-type': 'application/javascript'});
-        res.end(sourcedata);
-    }
-    console.log('conn to ' + req.url);
-}).listen(listeningport, function() {
-    console.log('Listening at: http://localhost:' + listeningport);
-});
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
 
 config.io = socketio.listen(server);
 
